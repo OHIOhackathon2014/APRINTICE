3b008b47c19bd254c06bd9fd7523298651bcc04e
bhead9318 <bhead9318@aim.com>
2014-10-04 17:44:26 -0400

    Added HTML and CSS front-end design template

---
+++ html/index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta name="robots" content="noindex, nofollow">
+<title>APRINTICE</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<div id="logo"></div>
+			<div id="welcome">Welcome, {username}</div>
+			<div id="logout"></div>
+		</div>
+		<div id="main">
+		
+			<!-- Job 1 -->
+			<div class="printjob">
+				<div class="jobtitle">Document1.docx</div>
+				<div class="jobuser">w022beh</div>
+				<div class="jobdate">
+					<div class="jobdatecontainer">
+						<div class="jobdatemonth">OCT</div>
+						<div class="jobdateday">4</div>
+					</div>
+					3:20:56 PM
+				</div>
+			</div>
+			
+			<div class="divider"></div>
+			
+			<!-- Job 2 -->
+			<div class="printjob">
+				<div class="jobtitle">Presentation 10-11-14.pdf</div>
+				<div class="jobuser">w629waw</div>
+				<div class="jobdate">
+					<div class="jobdatecontainer">
+						<div class="jobdatemonth">OCT</div>
+						<div class="jobdateday">3</div>
+					</div>
+					3:20:56 PM
+				</div>
+			</div>
+			
+			<div class="divider"></div>
+			
+			<!-- Job 3 -->
+			<div class="printjob">
+				<div class="jobtitle">Puppies.doc</div>
+				<div class="jobuser">w039amc</div>
+				<div class="jobdate">
+					<div class="jobdatecontainer">
+						<div class="jobdatemonth">OCT</div>
+						<div class="jobdateday">3</div>
+					</div>
+					3:19:43 PM
+				</div>
+			</div>
+			
+			<div class="divider"></div>
+			
+			<!-- Job 4 -->
+			<div class="printjob">
+				<div class="jobtitle">Spreadsheet7.xlsx</div>
+				<div class="jobuser">w022beh</div>
+				<div class="jobdate">
+					<div class="jobdatecontainer">
+						<div class="jobdatemonth">OCT</div>
+						<div class="jobdateday">3</div>
+					</div>
+					1:14:38 AM
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div id="footer-container">
+		<div id="footer">
+			<div id="balance-title">Your Balance:</div>
+			<div id="balance">
+				<sup>&#36;</sup>
+				0.00
+			</div>
+			<div id="preview"></div>
+			<div id="print"></div>
+		</div>
+	</div>
+</body>
+</html>
