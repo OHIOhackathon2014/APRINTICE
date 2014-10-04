e9d003d7f000d168d72d6cfedd43db47086a1233
Wes Weber <wweber@wesweber.com>
2014-10-04 16:06:39 -0400

    Test deleting jobs

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 from .models import DBSession, Job
+from pyramid.httpexceptions import HTTPSeeOther
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
@@ -12,3 +13,17 @@ def listjobs(req):
     q = DBSession.query(Job)
 
     return {"jobs": q.all()}
+
+@view_config(route_name="deletejob")
+def deletejob(req):
+    q = DBSession.query(Job)
+    q = q.filter(Job.id == req.matchdict["id"])
+    job = q.first()
+
+    try:
+        job.delete()
+        DBSession.delete(job)
+    except OSError:
+        pass
+
+    return HTTPSeeOther("/listjobs")
