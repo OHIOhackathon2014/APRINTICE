3203dd7e51142e8452164bcd21345d5367356cbc
Wes Weber <wweber@wesweber.com>
2014-10-04 14:24:02 -0400

    Session factory

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -2,6 +2,8 @@ from pyramid.config import Configurator
 
 from .models import configure_database, create_tables
 
+from pyramid.session import SignedCookieSessionFactory
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -11,6 +13,8 @@ def main(global_config, **settings):
 
     configure_database(settings["database"])
     create_tables()
+
+    config.set_session_factory(SignedCookieSessionFactory("TODO~CHANGEME"))
 
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
